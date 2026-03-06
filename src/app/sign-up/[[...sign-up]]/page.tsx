67e6eb8bddd95faa09ffc1e9dff6bb9f85260400
Andrés Felipe Tirano Vásquez <andres@tirano.co>
2026-03-06 15:47:50 -0500

    fix: mostrar formulario de registro en /sign-up

--- src/app/sign-up/[[...sign-up]]/page.tsx
+++ src/app/sign-up/[[...sign-up]]/page.tsx
@@ -1,10 +1,10 @@
-import { SignIn } from "@clerk/nextjs";
+import { SignUp } from "@clerk/nextjs";
 
 export default function Page() {
   return (
     <div className="min-h-screen w-full px-4 pt-28 pb-16 md:pt-36 flex justify-center">
       <div className="w-full max-w-md">
-        <SignIn />
+        <SignUp />
       </div>
     </div>
   );
